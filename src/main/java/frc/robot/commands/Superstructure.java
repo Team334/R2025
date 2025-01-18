f908992b8af2bac280576cf8cbf2c609749376ac
PGgit08 <peter.mty8@gmail.com>
2025-01-18 17:09:10 -0500

    super structure methods for ground intake/outtake and passoff

--- src/main/java/frc/robot/commands/Superstructure.java
+++ src/main/java/frc/robot/commands/Superstructure.java
@@ -4,6 +4,13 @@
 
 package frc.robot.commands;
 
+import static edu.wpi.first.wpilibj2.command.Commands.*;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Manipulator;
+import frc.robot.subsystems.Serializer;
+
 /**
  * Contains factory methods for commands that involve the whole superstructure of the robot (require
  * multiple subsystems).
@@ -11,5 +18,20 @@ package frc.robot.commands;
 public class Superstructure {
   private Superstructure() {
     throw new UnsupportedOperationException("This is a utility class!");
+  }
+
+  /** Passoff from intake (if needed) -> serializer -> manipulator. */
+  public Command passoff(Intake intake, Serializer serializer, Manipulator manipulator) {
+    return none();
+  }
+
+  /** Outtake from serializer -> intake. */
+  public Command groundOuttake(Intake intake, Serializer serializer) {
+    return none();
+  }
+
+  /** Intake from intake -> serializer. */
+  public Command groundIntake(Intake intake, Serializer serializer) {
+    return none();
   }
 }
