3934d6d169cb5f82ddad2ca15c0367ae64f1346a
PGgit08 <peter.mty8@gmail.com>
2025-01-18 15:00:50 -0500

    spot apply

--- src/main/java/frc/robot/utils/GlobalState.java
+++ src/main/java/frc/robot/utils/GlobalState.java
@@ -3,13 +3,13 @@ package frc.robot.utils;
 import frc.robot.subsystems.Manipulator.Piece;
 
 public class GlobalState {
-    private static Piece _currentPiece = Piece.NONE;
+  private static Piece _currentPiece = Piece.NONE;
 
-    public static Piece getCurrentPiece() {
-        return _currentPiece;
-    }
+  public static Piece getCurrentPiece() {
+    return _currentPiece;
+  }
 
-    public static void setCurrentPiece(Piece piece) {
-        _currentPiece = piece;
-    }
+  public static void setCurrentPiece(Piece piece) {
+    _currentPiece = piece;
+  }
 }
