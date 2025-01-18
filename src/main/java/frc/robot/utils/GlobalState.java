213221626e79a6deffdafaca9d618b8ce63522da
PGgit08 <peter.mty8@gmail.com>
2025-01-18 15:08:53 -0500

    updated global state

--- src/main/java/frc/robot/utils/GlobalState.java
+++ src/main/java/frc/robot/utils/GlobalState.java
@@ -1,15 +1,27 @@
 package frc.robot.utils;
 
 import frc.robot.subsystems.Manipulator.Piece;
+import frc.robot.subsystems.Wristevator.WristevatorSetpoint;
 
 public class GlobalState {
   private static Piece _currentPiece = Piece.NONE;
+  private static WristevatorSetpoint _setpoint = WristevatorSetpoint.HOME;
 
+  /** The current piece in the manipulator. */
   public static Piece getCurrentPiece() {
     return _currentPiece;
   }
 
   public static void setCurrentPiece(Piece piece) {
     _currentPiece = piece;
+  }
+
+  /** The current setpoint of the wristevator. */
+  public static WristevatorSetpoint getSetpoint() {
+    return _setpoint;
+  }
+
+  public static void setSetpoint(WristevatorSetpoint setpoint) {
+    _setpoint = setpoint;
   }
 }
