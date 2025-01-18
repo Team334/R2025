fa598505588eb4d530736491d36bc98572cb122b
Peleh Liu <liupeleh@gmai.com>
2025-01-18 11:42:03 -0500

    added logging Co-author-by: chrnso cherine6150@gmail.com

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -1,3 +1,23 @@
 package frc.robot.subsystems;
 
-public class Manipulator {}
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.lib.AdvancedSubsystem;
+
+public class Manipulator extends AdvancedSubsystem {
+  public Manipulator() {
+    setDefaultCommand(setSpeed(0));
+  }
+
+  public Command setSpeed(double speed) {
+    return run(() -> {}); // return rad/s
+  }
+
+  @Logged(name = "Speed")
+  public double getSpeed() {
+    return 0; // returns wheel speed in rad/s
+  }
+
+  @Override
+  public void close() throws Exception {}
+}
