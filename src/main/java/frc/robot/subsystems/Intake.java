fb55454c09b8fe5dac3766e29371a6a59fa9e274
Jerry <zeruiz@nycstudents.net>
2025-01-18 11:27:50 -0500

    Intake Basics
    Co-authored-by: rzhu-0812 rzhu0812@gmail.com

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,8 +1,29 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.AdvancedSubsystem;
 
 public class Intake extends AdvancedSubsystem {
+
+  public Intake() {
+    setDefaultCommand(set(0.0, 0.0));
+  }
+
+  public Command set(double actuatorAngle, double feedSpeed) {
+    return run(() -> {});
+  }
+
+  @Logged(name = "Speed")
+  public double getSpeed() {
+    return 0.0;
+  }
+
+  @Logged(name = "Angle")
+  public double getAngle() {
+    return 0.0;
+  }
+
   @Override
   public void close() throws Exception {}
 }
