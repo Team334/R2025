ee80303a8d26cf58242a5c97a9d6b1d618357269
PGgit08 <peter.mty8@gmail.com>
2025-01-17 22:37:58 -0500

    added empty core subsystem classes

---
+++ src/main/java/frc/robot/subsystems/Serializer.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import frc.lib.AdvancedSubsystem;
+
+public class Serializer extends AdvancedSubsystem {
+    @Override
+    public void close() throws Exception {
+        
+    }
+}
