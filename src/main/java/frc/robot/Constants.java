e1fcaed50b8f4b2d3b7747d2ccfc87bd6680f6a6
Elvis <gamerprotime777@gmail.com>
2025-03-12 17:52:45 -0400

    upper camer placement

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,6 +17,7 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.AngularAccelerationUnit;
 import edu.wpi.first.units.AngularVelocityUnit;
 import edu.wpi.first.units.VoltageUnit;
@@ -135,7 +136,7 @@ public final class Constants {
         new VisionPoseEstimatorConstants(
             upperLeftArducamName,
             new Transform3d(
-                new Translation3d(0.15, 0.3, 1), new Rotation3d(0, -0.08, Math.PI + 0.2)),
+                new Translation3d(0.154, 0.273, 0.809), new Rotation3d(0, -Units.degreesToRadians(10), Math.PI)),
             0.2,
             0.0001,
             3,
@@ -145,7 +146,7 @@ public final class Constants {
         new VisionPoseEstimatorConstants(
             upperRightArducamName,
             new Transform3d(
-                new Translation3d(0.15, -0.3, 1), new Rotation3d(0, -0.08, Math.PI - 0.2)),
+                new Translation3d(0.154, -0.273, 0.809), new Rotation3d(0, -Units.degreesToRadians(10), Math.PI)),
             0.2,
             0.0001,
             3,
