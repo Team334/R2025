ec9d661f5e1b0205300c0baa7104ce3853dd5f94
Elvis <gamerprotime777@gmail.com>
2025-03-24 17:40:23 -0400

    1pAUTO meowjectory

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,22 +4,43 @@
 
 package frc.robot.commands;
 
-import static edu.wpi.first.wpilibj2.command.Commands.*;
+import static edu.wpi.first.wpilibj2.command.Commands.sequence;
+import static frc.robot.Constants.WristevatorConstants.Preset.HUMAN;
+import static frc.robot.Constants.WristevatorConstants.Preset.L2;
+import static frc.robot.Constants.WristevatorConstants.Preset.L4;
+
+import java.util.function.Consumer;
 
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import dev.doglog.DogLog;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Manipulator;
+import frc.robot.subsystems.Manipulator.Piece;
+import frc.robot.subsystems.Serializer;
 import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Wristevator;
 
 public class Autos {
   private final Swerve _swerve;
+  private final Consumer<Piece> _currentPieceSetter;
+  private final Wristevator _wristevator;
+  private final Manipulator _manipulator;
+  private final Intake _intake;
+  private final Serializer _serializer;
 
   private final AutoFactory _factory;
 
   private boolean _seesPiece = false;
 
-  public Autos(Swerve swerve) {
+  public Autos(Swerve swerve, Consumer<Piece> currentPieceSetter, Wristevator wristevator, Manipulator manipulator, Intake intake, Serializer serializer) {
     _swerve = swerve;
+    _currentPieceSetter = currentPieceSetter;
+    _wristevator = wristevator;
+    _manipulator = manipulator;
+    _intake = intake;
+    _serializer = serializer;
 
     _factory =
         new AutoFactory(
@@ -36,17 +57,25 @@ public class Autos {
               DogLog.log("Auto/Current Trajectory Duration", traj.getTotalTime());
               DogLog.log("Auto/Current Trajectory Is Active", isActive);
             });
+      
+    _factory.
+    bind("Ground Intake", Superstructure.groundIntake(_intake, _serializer)).
+    bind("L4", wristevator.setGoal(L4)).
+    bind("L2", wristevator.setGoal(L2)).
+    bind("Human", wristevator.setGoal(HUMAN)).
+    bind("Manipulator Intake", _manipulator.intake().withTimeout(1.5)).
+    bind("Manipulator Outtake", _manipulator.outtake().withTimeout(1.5));
   }
 
-  public AutoRoutine simpleTrajectory() {
-    var routine = _factory.newRoutine("Simple Trajectory");
+  public AutoRoutine ground3P() {
+    var routine = _factory.newRoutine("Ground 3 Piece");
     var start = routine.trajectory("Start-Reef");
     var humanToReef1 = routine.trajectory("Human-Reef1");
     var reefToHuman1 = routine.trajectory("Reef-Human1");
     var humanToReef2 = routine.trajectory("Human-Reef2");
     var reefToHuman2 = routine.trajectory("Reef-Human2");
 
-    routine.active().onTrue(sequence(start.resetOdometry(), start.cmd()));
+    routine.active().onTrue(sequence(Commands.runOnce(() -> _currentPieceSetter.accept(Piece.CORAL)), start.resetOdometry(), start.cmd()));
 
     start.done().onTrue(reefToHuman1.cmd());
 
@@ -70,6 +99,25 @@ public class Autos {
                 _swerve.driveTo(humanToReef2.getFinalPose().get()),
                 reefToHuman2.cmd()));
     reefToHuman2.done().onTrue(humanToReef2.cmd());
+
+    return routine;
+  }
+
+  public AutoRoutine simplePath() {
+    var routine = _factory.newRoutine("Simple Path");
+    var traj = routine.trajectory("SimplePath");
+
+    routine.active().onTrue(sequence(Commands.runOnce(() -> _currentPieceSetter.accept(Piece.CORAL)), traj.resetOdometry(), traj.cmd()));
+    
+    return routine;
+  }
+
+  public AutoRoutine onePiece() {
+    var routine = _factory.newRoutine("One Piece");
+    var meowjectory = routine.trajectory("Start-Reef(A)");
+    var meowjectory2 = routine.trajectory("Reef-Human(A)");
+
+    routine.active().onTrue(sequence(Commands.runOnce(() -> _currentPieceSetter.accept(Piece.CORAL)), meowjectory.resetOdometry(), meowjectory.cmd(), meowjectory2.cmd()));
 
     return routine;
   }
