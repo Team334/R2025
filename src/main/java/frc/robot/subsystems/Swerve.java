dfd13d14bf98c4cc42ae5aa1e290da0ce36bbe45
PGgit08 <peter.mty8@gmail.com>
2025-03-27 23:48:31 -0400

    updated swerve drive motor stator current limit and added a gyro reef reset button for practice field

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -36,6 +36,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -233,6 +234,9 @@ public class Swerve extends TunerSwerveDrivetrain implements Subsystem, SelfChec
           DogLog.log("Swerve/Odometry Success %", state.SuccessfulDaqs / totalDaqs * 100);
           DogLog.log("Swerve/Odometry Period", state.OdometryPeriod);
         });
+
+    SmartDashboard.putData(
+        "Practice Field Reset", Commands.runOnce(() -> resetRotation(Rotation2d.fromDegrees(120))));
 
     autonomous()
         .onTrue(Commands.runOnce(() -> _ignoreVisionEstimates = true))
